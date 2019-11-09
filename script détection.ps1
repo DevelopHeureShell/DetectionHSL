83014c756fa6e3d8889304682d2322f2d17deba9
Woomy4680-exe <woomy4680@gmail.com>
2019-11-09 13:04:26 +0100

    Update script détection.ps1

--- script détection.ps1
+++ script détection.ps1
@@ -17,12 +17,7 @@ Mail:woomy4680@gmail.com
 <#
 préparation des fichiers, dossiers et variables
 #>
-$path = "D:/detectionlog"
-cd D:/
-remove-item -recurse $path 
-$Bureau = "D:\Bureau"
 mkdir detectionlog 
-cd $path
 mkdir partie1
 mkdir partie2
 mkdir partie3
@@ -30,21 +25,17 @@ mkdir partie4
 cd ./partie2
 mkdir sfc
 mkdir dism
-cd $path
 <#Partie 1:logs de Windows Update+process et ping
 #>
-cd ./partie1           #se place dans le dossier de la partie 1
+cd ./partie1 #se place dans le dossier de la partie 1
 get-process > process.txt #liste les processus en arrière plan
 ping $toping > ping.txt #teste le réseau (et le dnsssss)
 Get-AppBackgroundTask > appbackground.txt #liste les application en arrière plan
 get-windowsupdatelog #crée un fichier journal de windows update
-cd %userprofile% #se déplace sur le Bureau
-move windowsupdate.log "D:/detectionlog/partie1" #déplace le journal de Windows update dans le dossier du scipt 
 Get-ComputerInfo > infopc.txt #Récupère beaucoup d'infos sur l'ordinateur
 Get-volume > volumeinfo.txt #informations sur les diques (volumes)
-move volumeinfo.txt "$path/partie1"
-move infopc.txt "$path/partie"
-
+move volumeinfo.txt 
+move infopc.txt 
 
 <#
 Partie 2:Vérification du système
@@ -56,6 +47,3 @@ SFC /verifyonly | Tee-Object -filepath "D:/detectionlog/partie1/sfc.txt"
 echo 'sfc fini'
 dism /online /cleanup-image /scanhealth >scanhealth.txt #utilise l'utilitaire dism pour vérifier certains fichiers système
 dism /online /cleanup-image /checkhealth >checkhealth.txt #autre vérification dism
-
-move scanhealth.txt 
-mobe checkhealth.txt
