0c45f1fa609df5c9f6c1859c0b338a07b084712f
Woomy4680-exe <woomy4680@gmail.com>
2019-11-10 18:59:01 +0100

    Grosses modfs

--- readme.txt
+++ readme.txt
@@ -1,1 +1,16 @@
-Bienvenue sur ce script, il a pour but de fournir un maximum d'informations utiles au dépannage de votre pc
+Bienvenue sur ce script, il a pour but de détecter pas mal d'infos sur votre ordinateur. Il est composé de plusieurs
+parties qui sont expliquées dans le readme. 
+/!\n'oubliez pas de lire le readme avant de lancer le script/!\
+Le script est prêt à l'emploi mais il est conseillé de lire le readme pour adapter certains paramètres.
+Vous pouvez utiliser une seule étape dans le dossier du script.
+--------------------------------------------------------------------------------------------------------------------------------
+Contact:
+Github(report bugs):https://github.com/Woomy4680-exe/Script-d-tection-hardware-software
+Discord:Woomy4680_exe#6538
+Discord du scipt:https://discord.gg/V57eUPX
+Telegram:https://t.me/woomy4680_exe
+Telegram du scipt:https://t.me/gitwoomy
+Reddit:Woomy4680_exe
+Mail:woomy4680@gmail.com
+----------------------------------------------------------------------------------------------------------------
+Remerciment à :Alexbzh35480#4395, Émie#9936
--- script_detection.ps1
+++ script_detection.ps1
@@ -9,7 +9,7 @@ mkdir partie2\dism
 mkdir partie3
 mkdir partie4
 
-<#Partie 1:logs de Windows Update+process et ping
+<#Partie 1:logs de Windows Update+diverses infos
 #>
 get-process > partie1\process.txt #liste les processus en arrière plan
 ping $toping > partie1\ping.txt #teste le réseau (et le dnsssss)
@@ -17,7 +17,8 @@ Get-AppBackgroundTask > partie1\appbackground.txt #liste les application en arri
 get-windowsupdatelog -LogPath partie1\windowsupdate.log #crée un fichier journal de windows update
 Get-ComputerInfo > partie1\infopc.txt #Récupère beaucoup d'infos sur l'ordinateur
 Get-volume > partie1\volumeinfo.txt #informations sur les diques (volumes)
-
+Get-WmiObject -Class Win32_Product > partie1\applications.txt
+Get-AppPackage > partie1\applicationstore.txt 
 <#
 Partie 2:Vérification du système
 #>
@@ -25,7 +26,7 @@ $prev = [console]::OutputEncoding
 [console]::OutputEncoding = [Text.Encoding]::Unicode
 SFC /verifyonly | Tee-Object -filepath partie2\sfc.txt
 [console]::OutputEncoding = $prev #utilise l'utilitaire sfc pour regarder l'état des fichiers système (aucune réparation) 
-echo sfc terminé
+echo sfc fini
 dism /online /cleanup-image /scanhealth > partie2\scanhealth.txt #utilise l'utilitaire dism pour vérifier certains fichiers système
 dism /online /cleanup-image /checkhealth > partie2\checkhealth.txt #autre vérification dism
 
@@ -33,4 +34,3 @@ dism /online /cleanup-image /checkhealth > partie2\checkhealth.txt #autre vérif
 Partie 3: Mises à jour de certains programmes (optionel)
 #>
 Update-help #Met à jour l'aide Powershell
-
