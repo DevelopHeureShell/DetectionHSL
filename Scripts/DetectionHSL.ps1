5abff85fdb9eb80c8715db6db58d6217fa40be32
Woomy4680-exe <57042741+Woomy4680-exe@users.noreply.github.com>
2019-11-25 17:29:04 +0100

    je me suis bourré dans le commit

--- Scripts/DetectionHSL.ps1
+++ Scripts/DetectionHSL.ps1
@@ -8,6 +8,7 @@ mkdir Partie1
 mkdir Partie2 
 mkdir Partie3
 mkdir Partie4
+mkdir partie5
 
 <#
 Partie 1: obtention des logs et des process ouverts
@@ -36,12 +37,20 @@ SFC /verifyonly | Tee-Object -filepath partie3\sfc.txt
 Dism /online /cleanup-image /scanhealth  | Tee-Object -filepath partie3\Scanhealth.txt
 Dism /online /cleanup-image /checkhealth | Tee-Object -filepath partie3\Checkhealth.txt
 chkdsk > partie3\chkdsk.txt
+<#
+Partie 4: Mises a jour de windows.
+#>
 Update-help
 Write-Output 'Si la commande a retourné des erreurs rien de grave.'
+install-Module -Name PSWindowsUpdate 
+import-module PSWindowsUpdate
+Get-WUInstall -AcceptAll –IgnoreReboot
+
+
 <#
-Patie 4: Test de performances.
+Patie 5: Test de performances.
 #>
-Winsat formal | Tee-Object -filepath partie4\Winsat.txt
+Winsat formal | Tee-Object -filepath partie5\Winsat.txt
 Set-location $PSScriptRoot
 Write-Output 'script terminé sans erreurs. Vous pouvez maintenant envoyer le zip pour le dépannage'
 timeout /t 180
