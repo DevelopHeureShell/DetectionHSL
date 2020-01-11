f601c86ec675f265a0321832335e98cd07e172f3
Woomy4680-exe <57042741+Woomy4680-exe@users.noreply.github.com>
2020-01-11 14:42:14 +0100

    pause

--- Start_HSL.cmd
+++ Start_HSL.cmd
@@ -16,3 +16,4 @@
 "%~dp0Scripts\Additional_exe\deb5.exe"
 @powershell.exe -noprofile -executionpolicy bypass -file "%~dp0./Scripts/partie5.ps1" -include "%~dp0./Scripts/Core.psm1"
 "%~dp0Scripts\Additional_exe\end.exe"
+pause
