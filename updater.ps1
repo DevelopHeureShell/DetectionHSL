e82f060c00f5d2cbf7c83c8cbfd6c1203c95beb4
Matteo <matzar1808@gmail.com>
2020-04-04 14:34:13 +0200

    Updater install good version

--- updater.ps1
+++ updater.ps1
@@ -1,6 +1,6 @@
 $Host.UI.RawUI.WindowTitle = "DetectionHSL Updater"
 Set-Location $env:TEMP 
-if(Test-path .\hslupdater){
+if (Test-path .\hslupdater) {
     Remove-item -recurse hslupdater
 }
 New-item -ItemType Directory -Name hslupdater | Out-Null
@@ -8,13 +8,13 @@ Set-Location hslupdater
 $actual = Get-Content $PSScriptRoot\config.json | ConvertFrom-Json 
 Invoke-webrequest -OutFile new.json https://raw.githubusercontent.com/Woomy4680-exe/DetectionHSL/master/config.json
 $new = Get-Content new.json | ConvertFrom-Json 
-if($new.version -eq $actual.version){
+if ($new.version -eq $actual.version) {
     Write-Output "Already up to date"
     Write-Output "Press any key to exit"
     $Host.UI.RawUI.ReadKey() | Out-Null   
     exit 
 }
-else{
+else {
     
     $newver = $new.version.tostring()
     Write-Output "Updating DetectionHSL"
@@ -27,14 +27,15 @@ else{
     Set-Location $PSScriptRoot
     $instructions = Get-Content $newhsl\updater.json | ConvertFrom-Json
 
-    foreach($file in $instructions.files ){
-        Remove-item -Recurse -Force $file 
+    foreach ($file in $instructions.files ) {
+        if (Test-path $psscriptroot\$file) { Remove-item -Recurse -Force $file }
         Copy-Item -Recurse -Force $newhsl\$file $PSScriptRoot
     }
     Set-location scripts
     $mods = Get-ChildItem $newhsl\scripts
-    foreach($mod in $mods){
-        Remove-item -Recurse $mod 
+    foreach ($mod in $mods) {
+        if (Test-Path $PSScriptRoot\scripts\$mod) { Remove-item -Recurse $mod }
+        
         Copy-item -Recurse $newhsl\scripts\$mod $PSScriptRoot\scripts
     }
     Write-Output "Press any key to exit"
