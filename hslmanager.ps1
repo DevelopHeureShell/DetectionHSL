9ec988b12206340c628280858beacd7959004d05
Matteo <matzar1808@gmail.com>
2020-04-28 15:49:47 +0200

    Normalement c'est bon

--- hslmanager.ps1
+++ hslmanager.ps1
@@ -3,13 +3,13 @@ param (
     [switch]$accpetall = $false
 )
 $hslconf = Get-Content $PSScriptRoot\config.json | ConvertFrom-Json #On load la config
-if(!($nogui)){
-Clear-Host
-$text = "DetectionHSL Manager version " + $hslconf.version
-Write-Host ''
-Write-Host "-------------"
-Write-Host $text
-Write-Host "-------------"
+if (!($nogui)) {
+    Clear-Host
+    $text = "DetectionHSL Manager version " + $hslconf.version
+    Write-Host ''
+    Write-Host "-------------"
+    Write-Host $text
+    Write-Host "-------------"
 }
 if ($null -eq $args[0] -or $args[0] -eq "help" -or $args[0] -eq '?') {
     #On passe en mode HELP si y'a pas d'argss
@@ -26,44 +26,23 @@ elseif ($args[0] -eq 'enable') {
 elseif ($args[0] -eq 'disable') {
     $execmode = 'disable'
 }
-elseif($args[0] -eq 'update'){
+elseif ($args[0] -eq 'update') {
     $execmode = 'update'
 }
-elseif($args[0] -eq 'clean'){
+elseif ($args[0] -eq 'clean') {
     $execmode = 'clean'
 }
-elseif($args[0] -eq 'backup'){
+elseif ($args[0] -eq 'backup') {
     $execmode = 'backup'
 }
-elseif ($args[0] -eq 'restore'){
+elseif ($args[0] -eq 'restore') {
     $execmode = "restore"
 }
 else {
     Write-Output "Arguments invalides"
     $execmode = 'help'
 }
-
-if ($execmode -eq "disable"){
-    if ($null  -eq $args[1]){
-        Write-Host "Il faut indiquer un module"
-        Write-Host ''
-        Write-Host ''
-        exit 1
-    }
-    $newmods = @()
-    foreach ($mod in $hslconf.modules) {
-        if(!($mod -eq $args[1])){
-            $newmods += $mod
-        }
-    }
-    $hslconf.modules = $newmods
-    if(Test-path $PSScriptRoot\oldconfig.json){ Remove-Item -recurse oldconfig.json}
-    Rename-Item $PSScriptRoot\config.json oldconfig.json
-    $hslconf | ConvertTo-Json  > $PSScriptRoot\config.json
-    exit
-} #Si on est en disable il exit, un peu plus opti
-
-if($execmode -eq 'enable'){
+if ($execmode -eq "disable") {
     if ($null -eq $args[1]) {
         Write-Host "Il faut indiquer un module"
         Write-Host ''
@@ -71,8 +50,27 @@ if($execmode -eq 'enable'){
         exit 1
     }
     $newmods = @()
-    foreach($mod in $hslconf.modules){
-        if($args[1] -eq $mod){
+    foreach ($mod in $hslconf.modules) {
+        if (!($mod -eq $args[1])) {
+            $newmods += $mod
+        }
+    }
+    $hslconf.modules = $newmods
+    if (Test-path $PSScriptRoot\oldconfig.json) { Remove-Item -recurse oldconfig.json }
+    Rename-Item $PSScriptRoot\config.json oldconfig.json
+    $hslconf | ConvertTo-Json  > $PSScriptRoot\config.json
+    exit
+} 
+if ($execmode -eq 'enable') {
+    if ($null -eq $args[1]) {
+        Write-Host "Il faut indiquer un module"
+        Write-Host ''
+        Write-Host ''
+        exit 1
+    }
+    $newmods = @()
+    foreach ($mod in $hslconf.modules) {
+        if ($args[1] -eq $mod) {
             Write-Host "Module deja actif"
             exit 1
         }
@@ -85,28 +83,26 @@ if($execmode -eq 'enable'){
     $hslconf | ConvertTo-Json  > $PSScriptRoot\config.json
     exit
 }
-if ($execmode -eq 'start'){
+if ($execmode -eq 'start') {
     & $PSScriptRoot\start.bat
     exit
 }
-if($execmode -eq 'clean'){
+if ($execmode -eq 'clean') {
     $files = @(
         "hsllog.log",
         "oldconfig.json",
         "DetectionHSL.zip",
         "backup"
     )
-    foreach($file in $files){
-        if(Test-Path $PSScriptRoot\$file){
+    foreach ($file in $files) {
+        if (Test-Path $PSScriptRoot\$file) {
             Remove-item -Recurse $PSScriptRoot\$file
         }
     }
     exit
 }
-if ($execmode -eq 'help'){
+if ($execmode -eq 'help') {
     Write-Host -ForegroundColor Green "Aide de DetectionHSL"
-    Write-Host ""
-    Write-Host "Utilisation : hslmanager [commande]"
     Write-Host ""
     Write-Host "Clean: Nettoyer les fichiers inutiles de DetectionHSL"
     Write-Host "UTILISATION: hslmanager clean"
@@ -120,14 +116,20 @@ if ($execmode -eq 'help'){
     Write-Host "Start: Lancer DetectionHSL"
     Write-Host "UTILISATION: hslmanager start"
     Write-Host ""
+    Write-Host "Restore: Restaurer une sauvegarde"
+    Write-Host "UTILISATION: hslmanager restore [sauvegarde]"
+    Write-Host ""
+    Write-Host "Backup: Faire une sauvegarde"
+    Write-Host "UTILISATION: hslmanager backup [nom]"
+    Write-Host ""
 }
-if($execmode -eq 'backup'){
-    if($null -eq $args[1]){return}
+if ($execmode -eq 'backup') {
+    if ($null -eq $args[1]) { return }
     $name = $args[1]
-    if (Test-Path $PSScriptRoot\backup\$name){
+    if (Test-Path $PSScriptRoot\backup\$name) {
         Remove-Item -Recurse $PSScriptRoot\backup\$name
     }
-    if(!(Test-Path $PSScriptRoot\backup)){
+    if (!(Test-Path $PSScriptRoot\backup)) {
         New-Item -ItemType Directory -Path $PSScriptRoot\backup | Out-Null
     }
 
@@ -149,32 +151,50 @@ if($execmode -eq 'backup'){
         'hslmanager.ps1'
     )
     New-Item -ItemType Directory -Path $PSScriptRoot\backup\$name | Out-Null #Creation du dossier de la sauvegarde
-    foreach($file in $tobackup){
+    foreach ($file in $tobackup) {
         Copy-Item -Recurse $PSScriptRoot\$file $PSScriptRoot\backup\$name
     }
 }
-if ($execmode -eq 'restore'){
-    if ($null -eq $args[1]){
-        exit 1
-    }
+if ($execmode -eq 'restore') {
+    if ($null -eq $args[1]) { exit 1 }
     $name = $args[1]
-    if(!(Test-Path $PSScriptRoot\backup\$name)){
+    if (!(Test-Path $PSScriptRoot\backup\$name)) {
         Write-Host 'Sauvegarde invalide'
         exit 1
     }
-    if (!($accpetall)){
-        while(!($choice -eq 'y')){
+    if (!($accpetall)) {
+        while (!($choice -eq 'y')) {
             Write-Output "Etes vous sur de vouloir restaurer la sauvegarder $name ? [Y|N]"
             $choice = Read-Host 
-            if($choice.ToLower() -eq 'n' ){
+            if ($choice.ToLower() -eq 'n' ) {
                 Write-Host 'Sortie...'
                 exit
             }
         }
-        
+        $torestore = @(
+            "HSLlogo.png",
+            ".gitignore",
+            "config.json",
+            "start.bat", 
+            "update.bat",
+            "updater.json",
+            "updater.ps1",
+            "gui.ps1",
+            "scripts", 
+            "main",
+            "README.md", 
+            "LICENSE",
+            "loupe.ico",
+            'hslmanager.ps1')
+        foreach ($file in $torestore) {
+            if (Test-Path $PSScriptRoot\$file) { Remove-Item -Recurse $PSScriptRoot\$file }
+            Copy-Item -Recurse $PSScriptRoot\backup\$name\$file $PSScriptRoot\$file
+            Write-Host -ForegroundColor "Green" "OK!"
+        }
     }
 }
-if($execmode -eq 'update'){
+if ($execmode -eq 'update') {
     & $PSScriptRoot\hslmanager.ps1 backup bfrupdate
+    Write-Host -ForegroundColor Green "Mise a jour..."
     & $PSScriptRoot\updater.ps1 -accept
 }
