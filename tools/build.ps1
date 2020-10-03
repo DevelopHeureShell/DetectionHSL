7337d405a26da232e0753ad9bd0e6cb5316acade
Woomy4680-exe <matzar1808@gmail.com>
2020-10-03 19:01:09 +0200

    Added build script ++ moved manager l10n dir

--- tools/build.ps1
+++ tools/build.ps1
@@ -5,3 +5,13 @@ if(!(Test-path "$HOME/.cargo")) {
     Write-Host -ForegroundColor Red "Oops! Cargo is not installed!";
     exit 1;
 }
+Write-Host -ForegroundColor Green "Dependencies OK!";
+Set-Location "${PSScriptRoot}\..";
+$ROOT = Get-Location;
+Write-Host -ForegroundColor Yellow "Building HSL10N";
+Set-Location "${ROOT}/core/hsl10n";
+cargo build --release; 
+Remove-Item "${PWD}/target/release/hsl10n.d";
+Copy-Item "${PWD}/target/release/hsl10n*" "${ROOT}/bin";
+Write-Host -ForegroundColor Green "Done!";
+Set-Location $PSScriptRoot;
