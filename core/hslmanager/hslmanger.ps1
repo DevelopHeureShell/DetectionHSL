e953cb9c80a07d539756de6ce62e0d53e34fea51
Woomy4680-exe <matzar1808@gmail.com>
2020-10-03 19:10:16 +0200

    Fix HSLManager Start

--- core/hslmanager/hslmanger.ps1
+++ core/hslmanager/hslmanger.ps1
@@ -1,6 +1,7 @@
 #!/usr/bin/env pwsh
 $config = Get-content $PSScriptRoot\..\..\config.json | ConvertFrom-Json
-Write-Host "HSLManager " $config.version.type $config.version.major"."$config.version.minor"."$config.version.patch
+$bootstr = "HSLManager {0} V {1}.{2}.{3}" -f $config.version.type, $config.version.major, $config.version.minor, $config.version.patch
+Write-Host $bootstr
 if(!$args[0]) {
     Write-host -foregroundColor Red "Merci de spécifier la module à utiliser!";
 }
