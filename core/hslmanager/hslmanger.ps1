18f67d0121650bb013ab42c77a1bc1a009b6e9b7
Woomy4680-exe <matzar1808@gmail.com>
2020-10-03 18:43:43 +0200

    check cargo

--- core/hslmanager/hslmanger.ps1
+++ core/hslmanager/hslmanger.ps1
@@ -2,5 +2,5 @@
 $config = Get-content $PSScriptRoot\..\..\config.json | ConvertFrom-Json
 Write-Host "HSLManager " $config.version.type $config.version.major"."$config.version.minor"."$config.version.patch
 if(!$args[0]) {
-    Write-host -foregroundColor Red "Merci de spécifier la module à utiliser!"
+    Write-host -foregroundColor Red "Merci de spécifier la module à utiliser!";
 }
--- tools/build.ps1
+++ tools/build.ps1
@@ -1,3 +1,7 @@
 #!/usr/bin/env pwsh
-Write-Host -ForegroundColor Yellow "Building DetectioHSL..."
-Write-Host -ForegroundColor Blue "Checking Deps..."
+Write-Host -ForegroundColor Yellow "Building DetectioHSL...";
+Write-Host -ForegroundColor Blue "Checking Deps...";
+if(!(Test-path "$HOME/.cargo")) {
+    Write-Host -ForegroundColor Red "Oops! Cargo is not installed!";
+    exit 1;
+}
