0f9c077818969ccf5c8f74b949e8297304534605
Woomy4680-exe <matzar1808@gmail.com>
2020-10-04 11:04:33 +0200

    Fix HSLManager for not run in LInux

--- core/hslmanager/hslmanger.ps1
+++ core/hslmanager/hslmanger.ps1
@@ -1,5 +1,8 @@
 #!/usr/bin/env pwsh
 ## Import LANG module
+if(!($IsWindows)) {
+    return;
+}
 Import-Module "${PSScriptRoot}/../lib/lang.psm1"
 $config = Get-content "${PSScriptRoot}\..\..\config.json" | ConvertFrom-Json
 $bootstr = "HSLManager {0} V {1}.{2}.{3}" -f $config.version.type, $config.version.major, $config.version.minor, $config.version.patch
