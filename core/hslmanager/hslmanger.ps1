cb42fa40d72193e7cf92d7980b6761467ac0205e
Woomy4680-exe <matzar1808@gmail.com>
2020-10-04 10:59:26 +0200

    lang module now WORKING

--- core/hslmanager/hslmanger.ps1
+++ core/hslmanager/hslmanger.ps1
@@ -1,7 +1,9 @@
 #!/usr/bin/env pwsh
-$config = Get-content $PSScriptRoot\..\..\config.json | ConvertFrom-Json
+## Import LANG module
+Import-Module "${PSScriptRoot}/../lib/lang.psm1"
+$config = Get-content "${PSScriptRoot}\..\..\config.json" | ConvertFrom-Json
 $bootstr = "HSLManager {0} V {1}.{2}.{3}" -f $config.version.type, $config.version.major, $config.version.minor, $config.version.patch
 Write-Host $bootstr
 if(!$args[0]) {
-    Write-host -foregroundColor Red "Merci de spécifier la module à utiliser!";
+    Get-LangMessage -modname "manager" -msg "invalmod" -lang "en" 
 }
---
+++ core/lib/lang.psm1
@@ -0,0 +1,15 @@
+function Get-LangMessage {
+    param(
+        [string]$modname,
+        [string]$lang,
+        [string]$msg
+    )
+    $filepath = "${PSScriptRoot}/../l10n/${modname}/${lang}.yaml"
+    if(!(Test-Path $filepath)) {
+        $filepath = "${PSScriptRoot}/../l10n/${modname}/en.yaml"
+    }
+    if($IsWindows -and (Test-path "${PSScriptroot}\..\..\")){
+        & "${PSScriptRoot}\..\..\bin\hsl10n.exe" --file $filepath --msg $msg;
+    }
+}
+Export-ModuleMember Get-LangMessage;
