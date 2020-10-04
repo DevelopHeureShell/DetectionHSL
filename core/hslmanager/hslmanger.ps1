11f2d796b81912089c78829ff0aabf57ccee700b
Woomy4680-exe <matzar1808@gmail.com>
2020-10-04 11:48:03 +0200

    The fix is shit

--- core/hslmanager/hslmanger.ps1
+++ core/hslmanager/hslmanger.ps1
@@ -1,8 +1,5 @@
 #!/usr/bin/env pwsh
 ## Import LANG module
-if(!($IsWindows)) {
-    return;
-}
 Import-Module "${PSScriptRoot}/../lib/lang.psm1"
 $config = Get-content "${PSScriptRoot}\..\..\config.json" | ConvertFrom-Json
 $bootstr = "HSLManager {0} V {1}.{2}.{3}" -f $config.version.type, $config.version.major, $config.version.minor, $config.version.patch
---
+++ core/lib/packages.psm1
@@ -0,0 +1,5 @@
+function Install-SystemPackage {
+    # The Install-SystemPackage can be used to: Install a package in the running System
+
+}
+Export-ModuleMember Install-SystemPackage;
