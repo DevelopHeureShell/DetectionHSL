8bd5b663ff07dfc32e680b35dd9ad98422c18321
Woomy4680-exe <matzar1808@gmail.com>
2020-10-04 13:24:10 +0200

    Renamed lang module

--- core/hslmanager/hslmanger.ps1
+++ core/hslmanager/hslmanger.ps1
@@ -1,6 +1,6 @@
 #!/usr/bin/env pwsh
 ## Import LANG module
-Import-Module "${PSScriptRoot}/../lib/lang.psm1"
+Import-Module "${PSScriptRoot}/../lib/hslang.psm1"
 $config = Get-content "${PSScriptRoot}\..\..\config.json" | ConvertFrom-Json
 $bootstr = "HSLManager {0} V {1}.{2}.{3}" -f $config.version.type, $config.version.major, $config.version.minor, $config.version.patch
 Write-Host $bootstr
