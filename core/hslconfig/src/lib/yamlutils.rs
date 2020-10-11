4a080b3fb21d3104b12f78333cfb8ecdc674c58a
Rheydskey <matgag02@gmail.com>
2020-10-11 13:57:44 +0200

    Update yamlutils.rs

--- core/hslconfig/src/lib/yamlutils.rs
+++ core/hslconfig/src/lib/yamlutils.rs
@@ -1,5 +1,6 @@
 extern crate yaml_rust;
 use yaml_rust::{YamlLoader, Yaml};
 pub fn from_yaml(yamlstr: String) -> std::vec::Vec<Yaml> {
-    YamlLoader::load_from_str(&yamlstr).unwrap()
+    let docs = YamlLoader::load_from_str(&yamlstr).unwrap();
+    docs
 }
